1b5602f6b8d994d3f6b4b1ca7543022af5b8b4e8
Thalita Alves <thalitaalves783@outlook.com>
2022-11-13 14:28:37 -0300

    listagem de empresas

--- empresa/models.py
+++ empresa/models.py
@@ -29,6 +29,10 @@ class Empresa(models.Model):
     def __str__(self):
         return self.nome
 
+    def qtd_vagas(self):
+        return Vagas.objects.filter(empresa__id=self.id).count()
+
+
 class Vagas(models.Model):
     choices_experiencia = (
         ('J', 'Júnior'),
